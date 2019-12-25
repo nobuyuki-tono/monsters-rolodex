45f2c825dc76629cdb5e8f8d186eb17c7b328d34
Nobu <rockmusic2090@gmail.com>
2019-12-25 10:56:52 -0800

    Updates

---
+++ precache-manifest.d84e1555763c335012869f6463504a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d06fb4ac87f21a23252fefaea29191d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "969b38cda5bde36846c4",
+    "url": "/monsters-rolodex/static/css/main.4a6002de.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "969b38cda5bde36846c4",
+    "url": "/monsters-rolodex/static/js/main.5ed8a4f4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
